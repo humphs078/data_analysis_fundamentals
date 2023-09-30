0830629d6786b97fe42fd61b9569ba3601f23c39
humphs078 <sean@cruaghgreen.com>
2023-09-30 15:38:42 +0100

    Add collatz.py with exception handling

---
+++ tasks/scripts/collatz.py
@@ -0,0 +1,26 @@
+# collatz/py
+# Author: Sean Humphreys
+# Script to test the Collatz Conjecture from a range of integers read in from the command line as an argument.
+
+# import required libraries
+import collatz_functions as sh
+import sys
+
+try:
+    low_number = int(sys.argv[1])
+    high_number = int(sys.argv[2]) + 1
+    if low_number > 0 and low_number < high_number:
+        sh.result(low_number, high_number)
+    elif low_number > 0 and high_number < low_number:
+        print(f'{high_number} is lower than {low_number}. The script requires that the second argument is greater than'
+              f' the first. Please run the script again with the correct parameters.')
+    else:
+        print(f'{low_number} is not a positive integer. Please run the script again and enter a positive integer as '
+              f'an argument.')
+except ValueError:
+    print(f'Please enter a positive integer as an argument.')
+except IndexError:
+    print('Please enter two command line arguments. The arguments must be positive integers with the first number '
+          f'greater than the second number.')
+
+
