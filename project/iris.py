d21e0a62bf89e803198e4acc9a9c20c670dceec9
humphs078 <sean@cruaghgreen.com>
2023-10-13 15:31:19 +0100

    readin the dataset, rename the columns and remove Iris- string

--- project/iris.py
+++ project/iris.py
@@ -15,9 +15,17 @@ iris_ds_targets = iris.data.targets
 iris_ds_features['class'] = iris_ds_targets['class']
 print(type(iris_ds_features))
 
+# rename the DataFrame Columns
+# Or rename the existing DataFrame (rather than creating a copy) 
+iris_ds_features.rename(columns={'sepal length': 'sepal_length_cm', 'sepal width': 'sepal_width_cm', 'petal length': 'petal_length_cm', 'petal width': 'petal_width_cm', 'class': 'species'},  inplace=True)
+
+iris_ds_features['species'] = iris_ds_features['species'].str.replace('Iris-', '')
+
+print(iris_ds_features(head))
+
 # metadata 
 # print(iris.metadata) 
-  
+print(iris_ds_features)
 # variable information 
 print('\n')
-print(iris.variables)
+# print(iris.variables)
