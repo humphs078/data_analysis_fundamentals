23086e2296a0167b52d9dbdea3dbf25ffec139be
humphs078 <123559682+humphs078@users.noreply.github.com>
2023-10-25 11:38:01 +0100

    Task 2 - close-out. Add some info on variables to penguins.py.

--- tasks/task_2/penguins.py
+++ tasks/task_2/penguins.py
@@ -17,7 +17,12 @@ columns = penguins.shape[1]
 ds_type = type(penguins)
 null_values = penguins.isnull().sum()
 ds_5_rows = penguins.head()
+species = penguins['species'].unique()
+islands = penguins['island'].unique()
+sex = penguins['sex'].unique()
 
+
+# print formatted string with information on dataset
 print(f'\nThe {ds_name} consists of {rows} rows and {columns} columns.\n' 
       f'\nThe python DataType of the {ds_name} is {ds_type}.\n'
       f'\n{ds_name} Null Value summary: \n{null_values}\n'
@@ -29,3 +34,16 @@ penguins.info()
 # print the first five rows of the dataset
 print(f'\nThe first 5 rows of the {ds_name} are:\n'
       f'{ds_5_rows}')
+
+# print information on the categorical variables in the dataset
+print(f'\n***Categorical Variables***'
+      f'\nThe species of penguins in the \"species\" are '
+      f'{species[0]}, {species[1]} and {species[2]}.'
+      f'\nThe islands in the \"islands\" variable are'
+      f' {islands[0]}, {islands[1]} and {islands[2]}.'
+      f'\nThe sex variables are {sex[0]} and {sex[1]}.')
+
+# print information on the continuous variables
+print(f'\n***Continuous Variables***\n'
+      f'The continuous variables in the dataset are'
+      f' \"Bill Length\", \"Bill Depth" and \"Flipper Length\".')
