03257bd785fa344379a70f0686ba72601cc7c17e
humphs078 <sean@cruaghgreen.com>
2023-11-01 18:12:06 +0000

    Task 3 - numerical variable model

--- test.py
+++ test.py
@@ -6,3 +6,18 @@ import matplotlib.pyplot as plt
 # print(s)
 # plt.hist(x=s)
 # plt.show()
+
+# import the required software libraries
+import pandas as pd
+import seaborn as sns
+penguins = sns.load_dataset('penguins')
+
+summary = penguins.describe()
+print(summary)
+x = summary.loc['mean', 'bill_length_mm']
+print(x)
+print(type(x))
+print(penguins)
+# penguins = penguins.fillna(penguins.median())
+penguins[['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g']] = penguins[['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g']].fillna(penguins[['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g']].median())
+print(penguins)
